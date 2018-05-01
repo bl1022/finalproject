da84fa0006cea103a33d24fe521f64a915f41ee0
Jiawei Zhou <jiawei3@illinois.edu>
2018-04-30 19:44:35 -0500

    fixed all !!

--- app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package edu.illinois.cs.cs125.finalproject;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +10,22 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
     ImageButton androidImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(homeIntent);
+//                finish();
+//            }
+//
+//        },SPLASH_TIME_OUT);
         ImageButton button1 = (ImageButton) findViewById(R.id.imageButton8);
         ImageButton button2 = (ImageButton) findViewById(R.id.imageButton10);
         ImageButton button3 = (ImageButton) findViewById(R.id.imageButton12);
