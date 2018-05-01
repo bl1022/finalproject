b2f36bfe3eb058c078972b41e2a2247af0330c6d
Jiawei Zhou <jiawei3@illinois.edu>
2018-04-30 19:24:06 -0500

    fixed all 1

--- app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
@@ -15,34 +15,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        androidImageButton = (ImageButton) findViewById(R.id.imageButton8);
-        androidImageButton = (ImageButton) findViewById(R.id.imageButton10);
-        androidImageButton = (ImageButton) findViewById(R.id.imageButton12);
-        androidImageButton = (ImageButton) findViewById(R.id.imageButton16);
-        androidImageButton.setOnClickListener(new View.OnClickListener() {
+        ImageButton button1 = (ImageButton) findViewById(R.id.imageButton8);
+        ImageButton button2 = (ImageButton) findViewById(R.id.imageButton10);
+        ImageButton button3 = (ImageButton) findViewById(R.id.imageButton12);
+        ImageButton button4 = (ImageButton) findViewById(R.id.imageButton16);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.imageButton8:
-                    Intent intentLoadNewActivity = new Intent(MainActivity.this, Main2Activity.class);
-                    startActivity(intentLoadNewActivity);
-                    break;
-                    case R.id.imageButton10:
-                        Intent intentLoadNewActivity1 = new Intent(MainActivity.this, Main3Activity.class);
-                        startActivity(intentLoadNewActivity1);
-                        break;
-                    case R.id.imageButton12:
-                        Intent intentLoadNewActivity2 = new Intent(MainActivity.this, Main4Activity.class);
-                        startActivity(intentLoadNewActivity2);
-                        break;
-                    case R.id.imageButton16:
-                        Intent intentLoadNewActivity3 = new Intent(MainActivity.this, Main5Activity.class);
-                        startActivity(intentLoadNewActivity3);
-                        break;
-                }
+        Intent int1 = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(int1);
             }
-
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2 = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(int2);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int3 = new Intent(MainActivity.this, Main4Activity.class);
+                startActivity(int3);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int4 = new Intent(MainActivity.this, Main5Activity.class);
+                startActivity(int4);
+            }
+        });
+//        androidImageButton = (ImageButton) findViewById(R.id.imageButton8);
+//        androidImageButton = (ImageButton) findViewById(R.id.imageButton10);
+//        androidImageButton = (ImageButton) findViewById(R.id.imageButton12);
+//        androidImageButton = (ImageButton) findViewById(R.id.imageButton16);
+//        androidImageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switch (v.getId()) {
+//                    case R.id.imageButton8:
+//                    Intent intentLoadNewActivity = new Intent(MainActivity.this, Main2Activity.class);
+//                    startActivity(intentLoadNewActivity);
+//                    break;
+//                    case R.id.imageButton10:
+//                        Intent intentLoadNewActivity1 = new Intent(MainActivity.this, Main3Activity.class);
+//                        startActivity(intentLoadNewActivity1);
+//                        break;
+//                    case R.id.imageButton12:
+//                        Intent intentLoadNewActivity2 = new Intent(MainActivity.this, Main4Activity.class);
+//                        startActivity(intentLoadNewActivity2);
+//                        break;
+//                    case R.id.imageButton16:
+//                        Intent intentLoadNewActivity3 = new Intent(MainActivity.this, Main5Activity.class);
+//                        startActivity(intentLoadNewActivity3);
+//                        break;
+//                }
+//            }
+//
+//        });
     }
     public void open(View view) {
         Intent broseIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=2Vv-BfVoq4g&list=PLI_7Mg2Z_-4I-W_lI55D9lBUkC66ftHMg"));
